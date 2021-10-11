ce398a42986e927d5dadd3f357090ee9e8a6fec1
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-10-11 12:47:47 -0400

    Ajax and error messages.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,24 @@
 class StocksController < ApplicationController
 
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
+      else  
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid ticker symbol to search"
+          format.js { render partial: 'users/result' }
+        end
+      end
+    else   
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a ticker symbol to search"
+        format.js { render partial: 'users/result' }
+      end
+    end
   end
 
 end
